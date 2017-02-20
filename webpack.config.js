14b3c5afdc794a7d4469e91209219701295f2721
Jamie Starke <git@jamiestarke.com>
2017-02-19 22:15:21 -0800

    Moved the compiled resources to dist

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         __dirname: false
     },
     output: {
-        path: `./`,
+        path: `./dist/`,
         filename: 'index.js',
         libraryTarget: 'commonjs2'
     },
