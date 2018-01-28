51f1ef51dfd7d2303bd1427d94d74842e3261493
Kyle Poole <poolekyle@gmail.com>
2018-01-28 01:01:50 -0800

    Try to fix border overflow

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,34 +1,44 @@
 import * as express from "express";
 import * as _ from "lodash";
 import {
-  StartRequest, MoveRequest, StartResponse, MoveResponse, MoveResponseData,
+  StartRequest,
+  MoveRequest,
+  StartResponse,
+  MoveResponse,
+  MoveResponseData,
   StartResponseData,
   Snake,
   Point
-} from "../types/battlesnake"
+} from "../types/battlesnake";
 
 interface BattleSnakeRouter {
-  post(route: "/start", cb: (req: StartRequest, res: StartResponse) => any): void;
+  post(
+    route: "/start",
+    cb: (req: StartRequest, res: StartResponse) => any
+  ): void;
   post(route: "/move", cb: (req: MoveRequest, res: MoveResponse) => any): void;
 }
 
 const router: BattleSnakeRouter = express.Router();
 
 // Handle POST request to '/start'
-router.post('/start', (req: StartRequest, res: StartResponse): StartResponse => {
-  // NOTE: Do something here to start the game
-  const requestData = req.body;
+router.post(
+  "/start",
+  (req: StartRequest, res: StartResponse): StartResponse => {
+    // NOTE: Do something here to start the game
+    const requestData = req.body;
 
-  // Response data
-  const responseData: StartResponseData = {
-    color: "#CCCCFF",
-    name: "Periwinkle the Snake",
-    head_url: "http://www.placecage.com/c/200/200", // optional, but encouraged!
-    taunt: requestData.game_id // optional, but encouraged!
-  };
+    // Response data
+    const responseData: StartResponseData = {
+      color: "#CCCCFF",
+      name: "Periwinkle the Snake",
+      head_url: "http://www.placecage.com/c/200/200", // optional, but encouraged!
+      taunt: requestData.game_id // optional, but encouraged!
+    };
 
-  return res.json(responseData);
-});
+    return res.json(responseData);
+  }
+);
 
 function getMove(direction, invalidDirections: {}) {
   if (invalidDirections[direction]) return undefined;
@@ -37,7 +47,7 @@ function getMove(direction, invalidDirections: {}) {
 }
 
 // Handle POST request to '/move'
-router.post('/move', (req: MoveRequest, res: MoveResponse): MoveResponse => {
+router.post("/move", (req: MoveRequest, res: MoveResponse): MoveResponse => {
   const requestData = req.body;
   let move, taunt;
 
@@ -46,13 +56,16 @@ router.post('/move', (req: MoveRequest, res: MoveResponse): MoveResponse => {
     up: false,
     down: false,
     left: false,
-    right: false,
+    right: false
   };
 
   // Own snake data.
   const ownSnake: Snake = requestData.you;
   const snakeBody = ownSnake.body.data;
-  const { snakeHeadX, snakeHeadY } = { snakeHeadX: snakeBody[0].x, snakeHeadY: snakeBody[0].y };
+  const { snakeHeadX, snakeHeadY } = {
+    snakeHeadX: snakeBody[0].x,
+    snakeHeadY: snakeBody[0].y
+  };
 
   console.log(snakeHeadX);
   console.log(snakeHeadY);
@@ -60,14 +73,19 @@ router.post('/move', (req: MoveRequest, res: MoveResponse): MoveResponse => {
   // Check the snake's location in relation to the board.
   if (snakeHeadX === 0) invalidDirections.left = true;
   if (snakeHeadY === 0) invalidDirections.up = true;
-  if (snakeHeadX === requestData.width) invalidDirections.right = true;
-  if (snakeHeadY === requestData.height) invalidDirections.down = true;
+  if (snakeHeadX + 1 === requestData.width) invalidDirections.right = true;
+  if (snakeHeadY + 1 === requestData.height) invalidDirections.down = true;
 
   // Opposition.
-  const otherSnakes: Array<Snake> = _(requestData.snakes.data).omit({ id: ownSnake.id }).value();
-  const snakeBodies: Array<Point> = _(requestData.snakes.data).map(snake => snake.body.data).union().value();
+  const otherSnakes: Array<Snake> = _(requestData.snakes.data)
+    .omit({ id: ownSnake.id })
+    .value();
+  const snakeBodies: Array<Point> = _(requestData.snakes.data)
+    .map(snake => snake.body.data)
+    .union()
+    .value();
 
-  console.log('### OCCUPIED COORDS');
+  console.log("### OCCUPIED COORDS");
   console.log(snakeBodies);
 
   // Check the directions that we can go without hitting a snake.
@@ -77,15 +95,17 @@ router.post('/move', (req: MoveRequest, res: MoveResponse): MoveResponse => {
       if (snakeHeadX - 1 === point.x) invalidDirections.left = true;
       if (snakeHeadY + 1 === point.y) invalidDirections.down = true;
       if (snakeHeadY - 1 === point.y) invalidDirections.up = true;
-    })
+    });
   });
 
   console.log(invalidDirections);
 
   // Food
-  let closestFoodMoves = -1, closestFood;
+  let closestFoodMoves = -1,
+    closestFood;
   _.each(requestData.food.data, food => {
-    const moveCount =  Math.abs(snakeBody[0].x - food.x) + Math.abs(snakeBody[0].y - food.y);
+    const moveCount =
+      Math.abs(snakeBody[0].x - food.x) + Math.abs(snakeBody[0].y - food.y);
 
     if (closestFoodMoves < moveCount) {
       closestFoodMoves = moveCount;
@@ -98,38 +118,18 @@ router.post('/move', (req: MoveRequest, res: MoveResponse): MoveResponse => {
   _.each(otherSnakes, otherSnake => {
     const otherSnakeBody = otherSnake.body.data;
     if (otherSnakeBody.length >= ownSnake.length) return;
-    
-    const otherSnakeHead = otherSnakeBody[0]; 
 
-    if (_.inRange(snakeHeadX - 1, otherSnakeHead.x - 1, otherSnakeHead.x + 1)) 
+    const otherSnakeHead = otherSnakeBody[0];
+
+    if (_.inRange(snakeHeadX - 1, otherSnakeHead.x - 1, otherSnakeHead.x + 1))
       invalidDirections.left = true;
-    if (_.inRange(snakeHeadY - 1, otherSnakeHead.y - 1, otherSnakeHead.y + 1)) 
+    if (_.inRange(snakeHeadY - 1, otherSnakeHead.y - 1, otherSnakeHead.y + 1))
       invalidDirections.up = true;
-    if (_.inRange(snakeHeadX + 1, otherSnakeHead.x - 1, otherSnakeHead.x + 1)) 
+    if (_.inRange(snakeHeadX + 1, otherSnakeHead.x - 1, otherSnakeHead.x + 1))
       invalidDirections.right = true;
-    if (_.inRange(snakeHeadY + 1, otherSnakeHead.y - 1, otherSnakeHead.y + 1)) 
+    if (_.inRange(snakeHeadY + 1, otherSnakeHead.y - 1, otherSnakeHead.y + 1))
       invalidDirections.down = true;
   });
-
-  // If there is a food pellet nearby then grab it.
-  if (closestFood) {
-    if (snakeHeadX === closestFood.x) move = snakeHeadY < closestFood.y ? getMove('up', invalidDirections) : getMove('down', invalidDirections);
-    if (snakeHeadY === closestFood.y) move = snakeHeadX < closestFood.x ? getMove('right', invalidDirections) : getMove('left', invalidDirections);
-    
-    if (!move) {
-      let possibleDirections = [];
-
-      if (snakeHeadX < closestFood.x) possibleDirections.push('right')
-      else possibleDirections.push('left');
-
-      if (snakeHeadY < closestFood.y) possibleDirections.push('up')
-      else possibleDirections.push('down');
-
-      move = Math.random() < 0.5 ? getMove(possibleDirections[0], invalidDirections)  : getMove(possibleDirections[1], invalidDirections);
-    }
-
-    taunt = 'I\'m always hungry!';
-  }
 
   // If there is a less powerful snake within range possibly stop it.
   if (!move) {
@@ -139,32 +139,77 @@ router.post('/move', (req: MoveRequest, res: MoveResponse): MoveResponse => {
 
       const otherSnakeHead = otherSnakeBody[0];
 
-      if (_.inRange(snakeHeadX - 1, otherSnakeHead.x - 1, otherSnakeHead.x + 1) && snakeHeadY === otherSnakeHead.y) 
-        move = getMove('left', invalidDirections);
-      if (_.inRange(snakeHeadY - 1, otherSnakeHead.y - 1, otherSnakeHead.y + 1) && snakeHeadX === otherSnakeHead.x) 
-        move = getMove('down', invalidDirections);
-      if (_.inRange(snakeHeadX + 1, otherSnakeHead.x - 1, otherSnakeHead.x + 1) && snakeHeadY === otherSnakeHead.y) 
-        move = getMove('right', invalidDirections);
-      if (_.inRange(snakeHeadY + 1, otherSnakeHead.y - 1, otherSnakeHead.y + 1) && snakeHeadX === otherSnakeHead.x) 
-        move = getMove('up', invalidDirections);
+      if (
+        _.inRange(snakeHeadX - 1, otherSnakeHead.x - 1, otherSnakeHead.x + 1) &&
+        snakeHeadY === otherSnakeHead.y
+      )
+        move = getMove("left", invalidDirections);
+      if (
+        _.inRange(snakeHeadY - 1, otherSnakeHead.y - 1, otherSnakeHead.y + 1) &&
+        snakeHeadX === otherSnakeHead.x
+      )
+        move = getMove("down", invalidDirections);
+      if (
+        _.inRange(snakeHeadX + 1, otherSnakeHead.x - 1, otherSnakeHead.x + 1) &&
+        snakeHeadY === otherSnakeHead.y
+      )
+        move = getMove("right", invalidDirections);
+      if (
+        _.inRange(snakeHeadY + 1, otherSnakeHead.y - 1, otherSnakeHead.y + 1) &&
+        snakeHeadX === otherSnakeHead.x
+      )
+        move = getMove("up", invalidDirections);
 
-      taunt = 'I\'m coming for ya!';
+      taunt = "I'm coming for ya!";
     });
+  }
+
+  // If there is a food pellet nearby then grab it.
+  if (!move && closestFood) {
+    if (snakeHeadX === closestFood.x)
+      move =
+        snakeHeadY < closestFood.y
+          ? getMove("up", invalidDirections)
+          : getMove("down", invalidDirections);
+    if (snakeHeadY === closestFood.y)
+      move =
+        snakeHeadX < closestFood.x
+          ? getMove("right", invalidDirections)
+          : getMove("left", invalidDirections);
+
+    if (!move) {
+      let possibleDirections = [];
+
+      if (snakeHeadX < closestFood.x) possibleDirections.push("right");
+      else possibleDirections.push("left");
+
+      if (snakeHeadY < closestFood.y) possibleDirections.push("up");
+      else possibleDirections.push("down");
+
+      move =
+        Math.random() < 0.5
+          ? getMove(possibleDirections[0], invalidDirections)
+          : getMove(possibleDirections[1], invalidDirections);
+    }
+
+    taunt = "I'm always hungry!";
   }
 
   // For some reason we haven't settled on a move - randomly pick one direction that is allowed.
   if (!move && !_.every(invalidDirections, _.isTrue)) {
-    const validDirections = _.keys(_.pickBy(invalidDirections, direction => !direction));
-    move = validDirections[validDirections.length * Math.random() << 0];
-    
-    taunt = 'I\'m leaving this one up to fate!';
+    const validDirections = _.keys(
+      _.pickBy(invalidDirections, direction => !direction)
+    );
+    move = validDirections[(validDirections.length * Math.random()) << 0];
+
+    taunt = "I'm leaving this one up to fate!";
   }
 
   // If we don't have a move because no directions are valid then pick one randomly and post a suicide message.
   if (!move && _.every(invalidDirections, true)) {
-    move = ['up', 'down', 'left', 'right'][Math.floor(Math.random() * 4) + 1];
+    move = ["up", "down", "left", "right"][Math.floor(Math.random() * 4) + 1];
 
-    taunt = 'This is the end for me!'
+    taunt = "This is the end for me!";
   }
 
   // Response data
